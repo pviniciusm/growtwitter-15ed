7965399ce205715c26c42e3ee1b90aad62af70a0
Paulo <pvinics@gmail.com>
2024-01-16 20:08:32 -0300

    Configuração inicial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Hello world</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, hic.</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
